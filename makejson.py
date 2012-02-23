dc7e49539cb716dc5c1713c5eec225ba60f6d5a3
Michael Silveira <michael@gradecam.com>
2012-02-23 10:00:35 -0800

    fix to include those outlying places so that python script doesn't break on tenth zipcty file (and thus not missing all of california)

--- makejson.py
+++ makejson.py
@@ -6,13 +6,17 @@ import re
 statecodes = json.load(open('state_fips.json'))
 zipmap = {}
 
-for i in range(1,10):
-    zfile = open('zipctys/zipcty%d' % i)
+for i in range(1,11):
+    print 'line: ' + i
+    zfile = open('zipcty%d' % i)
     zfile.readline() # skip first line
     for l in zfile:
         m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)", l)       
         if m:
             r = m.groupdict()
             zipmap[r['zip']] = statecodes[r['state']] + r['fips']
+            
 
-print json.dumps(zipmap)
+f = open('zip2fips.json', 'w')
+json.dump(zipmap, f)
+f.close()
